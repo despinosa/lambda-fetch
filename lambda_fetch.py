56e1c608e14faaff0a4fafc6f9d0dd9d30327d89
despinosa <crypto.espinosa@gmail.com>
2019-07-14 18:23:10 -0500

    feat: add handler for relational backend

--- lambda_fetch.py
+++ lambda_fetch.py
@@ -12,15 +12,33 @@ The relational version assumes the following table exists:
 """
 
 
+import logging
 import os
 
 import sqlalchemy as sa
 
+
 CONN_STRING = os.getenv('CONN_STRING')
 
 ENGINE = sa.create_engine(CONN_STRING, max_overflow=5000)
+
+META = sa.MetaData(ENGINE, reflect=True)
+Tweet = META.tables(['tweet'])
+
 SESSION_FACTORY = sa.sessionmaker(bind=ENGINE)
 
 
+def fetch_from_relational(id: int) -> str:
+    session = sa.scoped_session(SESSION_FACTORY)
+    query = sa.select([Tweet.c.content]).where(Tweet.c.id == id)
+    logging.debug('executing select: %s', query)
+    content = session.execute(query)
+    logging.debug('returning: %s', content)
+    return content
+
+
 def handler(event, context):
-    raise NotImplementedError
+    if event['backend'] == 'relational':
+        return fetch_from_relational(event['id'])
+    else:
+        raise NotImplementedError
